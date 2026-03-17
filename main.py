0b882ab4539c6d91e4488d52f053991bfd09d410
Graham Long <grahamrlong@gmail.com>
2026-03-17 13:44:18 -0400

    Provided method for getting data from google sheet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
+import pandas as pd
+import warnings
+
 
 app = FastAPI()
 
@@ -9,5 +12,25 @@ def health_check():
 
 @app.get("/portfolio")
 def portfolio():
-    # implement portfolio getting and send to frontend
-    return {}
+    
+    url = "https://docs.google.com/spreadsheets/d/1JOmzegYf0kGxDcjej0Fx_RCuFVoaOxobEnDsptjTJA8/export?format=csv&gid=595987006"
+
+    df = pd.read_csv(url)
+
+    # looks for columns E:H 
+    df = df.iloc[:,[4,5,6,7]]
+
+    # drops the Weird ADBE and CRM Numbers at 
+    df.dropna(inplace = True)
+
+    for i in range(df.shape[1]):
+        warnings.filterwarnings('ignore')
+        df.rename(columns={df.columns[i]:df.iloc[0][i]}, inplace=True)
+
+    df.drop([0], inplace=True)
+
+    print(df.head())
+
+    return df
+
+portfolio()
